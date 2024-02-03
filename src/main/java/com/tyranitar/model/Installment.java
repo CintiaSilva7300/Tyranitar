df664fa3987ca117baac5f0b768d2bac9114de7b
Giovanni <giovannilima32@gmail.com>
2024-02-03 09:26:45 -0300

    Criar as models

---
+++ src/main/java/com/tyranitar/model/Installment.java
@@ -0,0 +1,27 @@
+package com.tyranitar.model;
+
+public class Installment {
+
+	private int installment_nunmber;
+	
+	private double value;
+
+	public int getInstallment_nunmber() {
+		return installment_nunmber;
+	}
+
+	public void setInstallment_nunmber(int installment_nunmber) {
+		this.installment_nunmber = installment_nunmber;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+	
+	
+	
+}
