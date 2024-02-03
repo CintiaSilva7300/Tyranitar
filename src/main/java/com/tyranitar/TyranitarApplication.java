c98d94ae752433f1b6bf5e9e4fed9fd6f8c14f2b
CintiaSilva7300 <88053580+CintiaSilva7300@users.noreply.github.com>
2024-02-03 08:47:52 -0300

    first commit

---
+++ src/main/java/com/tyranitar/TyranitarApplication.java
@@ -0,0 +1,13 @@
+package com.tyranitar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyranitarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TyranitarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tyranitar/TyranitarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyranitar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyranitarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
