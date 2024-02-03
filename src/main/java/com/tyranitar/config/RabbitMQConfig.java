6d34692ad0ea805953842f23aec79f0af03e387b
CintiaSilva7300 <88053580+CintiaSilva7300@users.noreply.github.com>
2024-02-03 13:08:40 -0300

    adicionar configuração para enviar a mensagem para o rabbitmq

---
+++ src/main/java/com/tyranitar/config/RabbitMQConfig.java
@@ -0,0 +1,15 @@
+package com.tyranitar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.Queue;
+
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public Queue productQueue() {
+        return new Queue("product", true); 
+    }
+}
